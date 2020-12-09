fa7449e3666bdd1faa761dacd0a4a28f8dfbb768
yoszimitsu <cotam07@gmail.com>
2020-12-09 18:07:40 +0100

    add MainPanelComponent and service

--- db-connection-app/src/app/app.module.ts
+++ db-connection-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
