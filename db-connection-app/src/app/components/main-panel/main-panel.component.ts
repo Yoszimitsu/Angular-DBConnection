68ad52ffea762cdf739c2ebcdb706fa0de9eb638
yoszimitsu <cotam07@gmail.com>
2020-12-09 23:13:10 +0100

    created logic in main-panel.component

--- db-connection-app/src/app/components/main-panel/main-panel.component.ts
+++ db-connection-app/src/app/components/main-panel/main-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UidService} from "../../services/uid.service";
+import {UidListService} from "../../services/uid-list.service";
 
 @Component({
   selector: 'app-main-panel',
@@ -7,9 +9,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPanelComponent implements OnInit {
 
-  constructor() { }
+  uid: string;
+  updated = false;
 
-  ngOnInit(): void {
+  uidObject = {
+    uid: '',
+    counter: 0,
+  };
+
+  result = {
+    uid: '',
+    counter: 0,
+  };
+
+  constructor(
+    private uidService: UidService,
+    private uidList: UidListService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.uid = this.uidList.getUid();
+    this.getUid(this.uid);
+  }
+
+  getUid(uid: string) {
+    this.uidService.get(uid)
+      .subscribe(
+        data => {
+          if (!data) {
+            this.uidObject.uid = this.uid;
+            this.uidObject.counter = 1;
+            this.addUid(this.uidObject.uid, this.uidObject);
+          } else if (!this.updated) {
+            this.uidObject.uid = data['uid'];
+            this.uidObject.counter = data['counter'] + 1
+            this.updateUid(data['uid'], this.uidObject);
+          } else {
+            this.result.uid = data['uid'];
+            this.result.counter = data['counter'];
+          }
+        },
+        error => {
+          console.log(error);
+        });
+  };
+
+  addUid(uid: string, uidObject: Object) {
+    this.uidService.create(uid, uidObject)
+      .subscribe(
+        response => {
+          console.log("The Uid was added successfully!");
+          this.result.uid = this.uidObject.uid;
+          this.result.counter = this.uidObject.counter;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateUid(uid: string, uidObject: Object) {
+    this.uidService.update(uid, uidObject)
+      .subscribe(
+        response => {
+          console.log('The Uid was updated successfully!');
+          this.updated = true;
+          this.getUid(this.uidObject.uid)
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
