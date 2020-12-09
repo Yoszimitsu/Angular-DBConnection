dfaa1841636d361635ee0e6594255b95d3447460
yoszimitsu <cotam07@gmail.com>
2020-12-09 18:07:56 +0100

    add routing

--- db-connection-app/src/app/app-routing.module.ts
+++ db-connection-app/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPanelComponent} from "./components/main-panel/main-panel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'uid', pathMatch: 'full'},
+  {path: 'uid', component: MainPanelComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
